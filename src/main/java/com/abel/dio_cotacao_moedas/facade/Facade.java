888aca264ca9db2338bd3f288f76329cdd57658f
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-04-16 17:29:34 -0300

    fix: facade dependencies removed autowride

--- src/main/java/com/abel/dio_cotacao_moedas/facade/Facade.java
+++ src/main/java/com/abel/dio_cotacao_moedas/facade/Facade.java
@@ -2,51 +2,58 @@ package com.abel.dio_cotacao_moedas.facade;
 
 import com.abel.dio_cotacao_moedas.dto.MoedaCotacao;
 import com.abel.dio_cotacao_moedas.model.Moeda;
+import com.abel.dio_cotacao_moedas.service.*;
 import org.springframework.http.ResponseEntity;
 
 public class Facade implements IFacade {
+    private final IServiceDollarReal serviceDollarReal = new ServiceDollarReal();
+
+    private final IServiceEuroReal serviceEuroReal = new ServiceEuroReal();
+
+    private final IServiceLibraReal serviceLibraReal = new ServiceLibraReal();
+
     @Override
     public ResponseEntity<Moeda> getDollarInReal() {
-        return null;
+        return serviceDollarReal.getDollarInReal();
     }
 
     @Override
     public ResponseEntity<Moeda> parseRealToDollar(MoedaCotacao moedaCotacao) {
-        return null;
+        return serviceDollarReal.parseRealToDollar(moedaCotacao);
     }
 
     @Override
     public ResponseEntity<Moeda> parseDollarToReal(MoedaCotacao moedaCotacao) {
-        return null;
+        return serviceDollarReal.parseDollarToReal(moedaCotacao);
     }
 
     @Override
     public ResponseEntity<Moeda> getEuroInReal() {
-        return null;
+        return serviceEuroReal.getEuroInReal();
     }
 
     @Override
     public ResponseEntity<Moeda> parseRealToEuro(MoedaCotacao moedaCotacao) {
-        return null;
+        return serviceEuroReal.parseRealToEuro(moedaCotacao);
     }
 
     @Override
     public ResponseEntity<Moeda> parseEuroToReal(MoedaCotacao moedaCotacao) {
-        return null;
+        return serviceEuroReal.parseEuroToReal(moedaCotacao);
     }
 
     @Override
     public ResponseEntity<Moeda> getLibraInReal() {
-        return null;
+        return serviceLibraReal.getLibraInReal();
     }
 
     @Override
     public ResponseEntity<Moeda> parseRealToLibra(MoedaCotacao moedaCotacao) {
-        return null;
+        return serviceLibraReal.parseRealToLibra(moedaCotacao);
     }
 
     @Override
     public ResponseEntity<Moeda> parseLibraToReal(MoedaCotacao moedaCotacao) {
-        return null;
+        return serviceLibraReal.parseLibraToReal(moedaCotacao);
     }
 }
